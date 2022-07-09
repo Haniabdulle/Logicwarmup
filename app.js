55b8b5bc90c515dd13987f8920b06cd49e5f2ee7
hani abdulle <haniamira24@gmail.com>
2022-07-08 23:18:34 -0600

    completed

---
+++ app.js
@@ -0,0 +1,20 @@
+var age = 18
+var is_subscribed=true;
+
+
+if (userAges < 18){
+    console.log("the user is younger than 18");
+}
+
+else{
+    console.log ("the user is not younger than 18");
+}
+
+if (userSubscribtion){
+    console.log("the user is subscribed");
+}
+
+else{
+    console.log("the user is not subscribed");
+}
+
